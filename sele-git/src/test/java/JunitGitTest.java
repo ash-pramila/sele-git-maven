f92f931ffa0edfaef8e24db67c7a9415fbf579fd
Pramila Palavesam Raju <pramilap0123@gmail.com>
2018-03-20 15:43:25 +0530

    committing files of the project

---
+++ sele-git/src/test/java/JunitGitTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class JunitGitTest {
+
+	@Before
+	public void setUp() throws Exception {
+		System.out.println("Setup entered");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		System.out.println("teardown entered");
+	}
+
+	@Test
+	public void test() {
+		System.out.println("Test entered");
+	}
+
+}
